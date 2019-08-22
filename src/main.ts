96d4360b355a1fb76387e7a25a986fc26dba2c71
Brendan Early <mymindstorm@evermiss.net>
2019-08-22 16:25:59 -0500

    fix it

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
-import { CodeGenerator } from '@babel/generator';
 
 async function run() {
   try {
     const emsdkArchive = await tc.downloadTool("https://github.com/emscripten-core/emsdk/archive/master.tar.gz");
     const emsdkFolder = await tc.extractTar(emsdkArchive);
     const emsdk = `${emsdkFolder}/emsdk-master/emsdk`
-    await exec.exec(emsdk, ["install latest",]);
-    await exec.exec(emsdk, ["activate latest"]);
-    await exec.exec(emsdk, ["construct_env"], {listeners: {
+    
+    await exec.exec(`${emsdk} install latest`);
+    await exec.exec(`${emsdk} activate latest`);
+    await exec.exec(`${emsdk} construct_env`, [], {listeners: {
       stdline(e) {
         core.warning(e);
       }
