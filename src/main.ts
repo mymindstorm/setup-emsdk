34d81df011888b88315dd0e0a195ba986ff25f0a
Brendan Early <mymindstorm@evermiss.net>
2019-08-22 15:58:27 -0500

    run using python

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function run() {
   try {
     const emsdkArchive = await tc.downloadTool("https://github.com/emscripten-core/emsdk/archive/master.tar.gz");
     const emsdkFolder = await tc.extractTar(emsdkArchive);
-    await exec.exec(`${emsdkFolder}/emsdk install latest`);
-    await exec.exec(`${emsdkFolder}/emsdk activate latest`);
-    await exec.exec(`source ${emsdkFolder}/emsdk_env.sh`);
+    await exec.exec(`python2.7 ${emsdkFolder}/emsdk install latest`);
+    await exec.exec(`python2.7 ${emsdkFolder}/emsdk activate latest`);
+    // await exec.exec(`source ${emsdkFolder}/emsdk_env.sh`);
   } catch (error) {
     core.setFailed(error.message);
   }
