0929b573bc16e34c71cc07e8f67a4f75725e9a7e
Brendan Early <mymindstorm@evermiss.net>
2019-08-22 18:40:40 -0500

    debug on main branch time

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,12 @@ async function run() {
 
     let emsdkFolder;
     let foundInCache = false;
-
+    
+    core.warning(typeof emArgs.noCache)
+    console.log(emArgs)
     if (emArgs.version !== "latest" && emArgs.noCache === "false") {
       emsdkFolder = tc.find('emsdk', emArgs.version, os.arch());
+      core.warning("a" + emsdkFolder);
     } 
 
     if (!emsdkFolder) {
