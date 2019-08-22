81c9b3fb9d1dd6adb75e463313490fc137038682
Brendan Early <mymindstorm@evermiss.net>
2019-08-22 16:39:59 -0500

    set vars accordingly

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,28 @@ async function run() {
     const emsdkArchive = await tc.downloadTool("https://github.com/emscripten-core/emsdk/archive/master.tar.gz");
     const emsdkFolder = await tc.extractTar(emsdkArchive);
     const emsdk = `${emsdkFolder}/emsdk-master/emsdk`
-    
+
     await exec.exec(`${emsdk} install latest`);
     await exec.exec(`${emsdk} activate latest`);
     await exec.exec(`${emsdk} construct_env`, [], {listeners: {
-      stdline(e) {
-        core.warning(e);
+      stdline(message) {
+        const pathRegex = new RegExp(/PATH \+= (\S+)/)
+        const pathResult = pathRegex.exec(message);
+
+        if (pathResult) {
+          core.addPath(pathResult[0]);
+          core.debug(`Add path: ${pathResult[0]}`);
+          return;
+        }
+        
+        const envRegex = new RegExp(/(\S+) = (\S+)/);
+        const envResult = envRegex.exec(message);
+
+        if (envResult) {
+          core.exportVariable(envResult[0], envResult[1]);
+          core.debug(`Set env: ${envResult[0]} = ${envResult[1]}`);
+          return;
+        }
       }
     }})
   } catch (error) {
