8de61b6c32f9e64972bb2c65f3e9a9a3105ed471
Brendan Early <mymindstorm@evermiss.net>
2019-08-22 15:23:59 -0500

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    await exec.exec("git clone https://github.com/emscripten-core/emsdk.git");
+    await exec.exec("cd emsdk");
+    await exec.exec("./emsdk install latest");
+    await exec.exec("./emsdk activate latest");
+    await exec.exec("source ./emsdk_env.sh");
   } catch (error) {
     core.setFailed(error.message);
   }
